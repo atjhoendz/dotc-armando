01ab7f1bf433694d1f45d4b67308d75eee1af288
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-27 13:09:48 +0000

    Deploy atjhoendz/dotc-armando to github.com/atjhoendz/dotc-armando.git:gh-pages

---
+++ precache-manifest.775b399d13d1f68672a2f7df7a2ddbb3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f691daba1026deb707d2226be44b0e90",
+    "url": "/dotc-armando/DariAkuUntukKamuSayangku.pdf"
+  },
+  {
+    "revision": "30198bd6d680915d5a2f",
+    "url": "/dotc-armando/css/chunk-vendors.77875be6.css"
+  },
+  {
+    "revision": "5502526028b277af8e8e7cfe6dfb9f40",
+    "url": "/dotc-armando/img/soal3.55025260.png"
+  },
+  {
+    "revision": "cc79c0ba15ec7c20929832e113ded08a",
+    "url": "/dotc-armando/index.html"
+  },
+  {
+    "revision": "bf2ac906f75d75c5e708",
+    "url": "/dotc-armando/js/app.29494cc1.js"
+  },
+  {
+    "revision": "30198bd6d680915d5a2f",
+    "url": "/dotc-armando/js/chunk-vendors.0a42aca4.js"
+  },
+  {
+    "revision": "679c4c3fb5bf6dbb2e766d1b6fde9e70",
+    "url": "/dotc-armando/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dotc-armando/robots.txt"
+  }
+]);
